7acc611456714911e117eb8ff8c4b9d3eed17336
Nicolas Decoopman <171740134+NCSdecoopman@users.noreply.github.com>
2025-11-07 11:18:09 +0100

    workflow terraform

--- export/main.tf
+++ export/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version = ">= 5.0" }
+    aws     = { source = "hashicorp/aws", version = ">= 5.0" }
     archive = { source = "hashicorp/archive", version = ">= 2.4.0" }
   }
 }
@@ -10,37 +10,66 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "aws_region" { default = "eu-west-3" }
-variable "table_name" { default = "Observations" }
-variable "repo_owner" { default = "NCSdecoopman" }
-variable "repo_name"  { default = "SnowViz" }
-variable "branch"     { default = "main" }
-variable "secret_name" { default = "SnowViz-AutoUpdate-AWS" }
+########################
+# Variables
+########################
+variable "aws_region" {
+  type    = string
+  default = "eu-west-3"
+}
+variable "table_name" {
+  type    = string
+  default = "Observations"
+}
+variable "repo_owner" {
+  type    = string
+  default = "NCSdecoopman"
+}
+variable "repo_name" {
+  type    = string
+  default = "SnowViz"
+}
+variable "branch" {
+  type    = string
+  default = "main"
+}
+variable "secret_name" {
+  type    = string
+  default = "SnowViz-AutoUpdate-AWS"
+}
+# Active le déclencheur EventBridge quotidien à 07:00 UTC si true -var="enable_schedule=true"
+variable "enable_schedule" {
+  type    = bool
+  default = false
+}
 
 data "aws_caller_identity" "me" {}
 data "aws_region" "cur" {}
 
-# Zip du code
+########################
+# Packaging Lambda
+########################
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "${path.module}/lambda_function.py"
   output_path = "${path.module}/lambda.zip"
 }
 
-# Rôle Lambda
+########################
+# IAM
+########################
 resource "aws_iam_role" "lambda_role" {
   name = "ddb-export-snowviz-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = { Service = "lambda.amazonaws.com" },
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-# Politique IAM minimale
 resource "aws_iam_policy" "lambda_policy" {
   name        = "ddb-export-snowviz-policy"
   description = "DynamoDB Scan, Secrets read, Logs"
@@ -48,22 +77,22 @@ resource "aws_iam_policy" "lambda_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid: "DdbScan",
-        Effect: "Allow",
-        Action: ["dynamodb:Scan"],
-        Resource: "arn:aws:dynamodb:${data.aws_region.cur.name}:${data.aws_caller_identity.me.account_id}:table/${var.table_name}"
+        Sid : "DdbScan",
+        Effect : "Allow",
+        Action : ["dynamodb:Scan"],
+        Resource : "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.me.account_id}:table/${var.table_name}"
       },
       {
-        Sid: "SecretsRead",
-        Effect: "Allow",
-        Action: ["secretsmanager:GetSecretValue"],
-        Resource: "arn:aws:secretsmanager:${data.aws_region.cur.name}:${data.aws_caller_identity.me.account_id}:secret:${var.secret_name}*"
+        Sid : "SecretsRead",
+        Effect : "Allow",
+        Action : ["secretsmanager:GetSecretValue"],
+        Resource : "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.me.account_id}:secret:${var.secret_name}*"
       },
       {
-        Sid: "Logs",
-        Effect: "Allow",
-        Action: ["logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents"],
-        Resource: "*"
+        Sid : "Logs",
+        Effect : "Allow",
+        Action : ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource : "*"
       }
     ]
   })
@@ -74,13 +103,17 @@ resource "aws_iam_role_policy_attachment" "attach" {
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
-# Log group (rétention 7 jours)
+########################
+# Logs
+########################
 resource "aws_cloudwatch_log_group" "lg" {
   name              = "/aws/lambda/ddb-export-observations-to-github"
   retention_in_days = 7
 }
 
-# Fonction Lambda
+########################
+# Lambda
+########################
 resource "aws_lambda_function" "exporter" {
   function_name = "ddb-export-observations-to-github"
   role          = aws_iam_role.lambda_role.arn
@@ -92,26 +125,29 @@ resource "aws_lambda_function" "exporter" {
 
   environment {
     variables = {
-      TABLE_NAME             = var.table_name
-      GH_OWNER               = var.repo_owner
-      GH_REPO                = var.repo_name
-      GH_BRANCH              = var.branch
-      GH_PATH                = "data/observations.json"
-      GH_TOKEN_SECRET_ARN    = "arn:aws:secretsmanager:${data.aws_region.cur.name}:${data.aws_caller_identity.me.account_id}:secret:${var.secret_name}"
-      DDB_PROJECTION         = "id,#d,HNEIGEF,NEIGETOT,NEIGETOT06"
-      MAX_JSON_MB            = "100"
-      FALLBACK_GZ_PATH       = "data/observations.json.gz"
+      TABLE_NAME          = var.table_name
+      GH_OWNER            = var.repo_owner
+      GH_REPO             = var.repo_name
+      GH_BRANCH           = var.branch
+      GH_PATH             = "data/observations.json"
+      GH_TOKEN_SECRET_ARN = "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.me.account_id}:secret:${var.secret_name}"
+      DDB_PROJECTION      = "id,#d,HNEIGEF,NEIGETOT,NEIGETOT06"
+      MAX_JSON_MB         = "100"
+      FALLBACK_GZ_PATH    = "data/observations.json.gz"
     }
   }
 
   depends_on = [aws_iam_role_policy_attachment.attach, aws_cloudwatch_log_group.lg]
 }
 
-# EventBridge: tous les jours à 07:00 UTC
-# => 08:00 à Paris en hiver (UTC+1), 09:00 en été (UTC+2)
+########################
+# EventBridge (optionnel)
+########################
+# cron(Minutes Heures Jour-du-mois Mois Jour-de-semaine Année)
+# 07:00 UTC tous les jours = cron(0 7 * * ? *) = 08:00 heure FR en hiver (09:00 en été)
 resource "aws_cloudwatch_event_rule" "daily" {
   name                = "ddb-export-observations-daily"
-  schedule_expression = "cron(30 8 * * ? *)"
+  schedule_expression = "cron(0 7 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "tgt" {
