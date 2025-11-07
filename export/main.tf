c5d0c740a8af96a75ce2ddf67e4790a12ee3ac78
Nicolas Decoopman <171740134+NCSdecoopman@users.noreply.github.com>
2025-11-07 09:16:56 +0100

    workflow terraform

--- export/main.tf
+++ export/main.tf
@@ -111,7 +111,7 @@ resource "aws_lambda_function" "exporter" {
 # => 08:00 à Paris en hiver (UTC+1), 09:00 en été (UTC+2)
 resource "aws_cloudwatch_event_rule" "daily" {
   name                = "ddb-export-observations-daily"
-  schedule_expression = "cron(00 8 * * ? *)"
+  schedule_expression = "cron(30 9 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "tgt" {
